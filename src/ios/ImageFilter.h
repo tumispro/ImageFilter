af646280a59cc09d95fead85e3ea827ccd60fe04
doomboy00 <henry@webratsmedia.co.uk>
2014-06-22 23:49:45 +0100

    Updating to Cordova 3

---
+++ src/ios/ImageFilter.h
@@ -0,0 +1,43 @@
+//
+//  ImageFilter.h
+//
+//  Created by Drew Dahlman 2/25/2012. 
+//  Copyright 2012 Drew Dahlman. All rights reserved.
+//
+//  Updated by Henry Ruhl 4/20/2014
+//
+//  version 1.1
+
+//  Current Filters:
+//  none
+//  stark
+//  sunnySide
+//  worn
+//  vintage
+
+/*
+Copyright (c) 2012 Drew Dahlman MIT LICENSE
+*/
+
+
+#import <Cordova/CDV.h>
+#import <Foundation/Foundation.h>
+
+@interface ImageFilter : CDVPlugin
+{
+	NSString* callbackID;
+}
+@end
+
+@property (nonatomic, retain) NSString* callbackID;
+
+// GARBAGE
+- (void)clean:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+// FILTERS
+- (void)none:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)stark:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)sunnySide:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)worn:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)vintage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+@end
